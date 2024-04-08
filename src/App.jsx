92e48b9f3d657b0ae580dae77e100ba6fe2c33bd
David Silvera <david4709djsr@gmail.com>
2024-04-08 01:23:54 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,18 @@ function App() {
         throw new Error(`API call failed with status: ${response.status}`); // Error si la respuesta no es exitosa.
       }
       const data = await response.json();
-      setCharacters(data.results || []);
+      // Si data.results es vacío, devuelve null "no encontrado"
+      if (data.results.length === 0) {
+        setCharacters(null); 
+      } else {
+        setCharacters(data.results);
+      }
     } catch (error) {
       console.error('Fetch error:', error);
-      alert('Ha ocurrido un error al cargar los personajes'); 
+      setCharacters(null); 
     }
   };
-
+  
   // Actualiza personajes cuando los filtros cambian.
   useEffect(() => {
     getCharacters(nameFilter, statusFilter);
@@ -36,13 +41,18 @@ function App() {
       <SearchAndFilter setNameFilter={setNameFilter} setStatusFilter={setStatusFilter} />
       <div className="container">
         <div id="characters">
-        {characters.length ? characters.map(character => (
-          <CharacterCard key={character.id} character={character} />
-        )) : <h2 id="mns-not-found">No se encontraron personajes con ese nombre.</h2>}
-        </div>
+          {characters === null ? (
+            <h2 id="mns-not-found">No se encontraron personajes con ese nombre.</h2>
+          ) : characters.length > 0 ? (
+            characters.map(character => (
+              <CharacterCard key={character.id} character={character} />
+            ))
+          ) : ( <h2 id="mns-not-found">Realiza una búsqueda para encontrar personajes.</h2>)}
+        </div>  
       </div>
     </>
   );
+  
 }
 
 export default App;
